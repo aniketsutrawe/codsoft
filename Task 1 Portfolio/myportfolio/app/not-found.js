4bd823deffdcc60fe4640bdb2c6d60eb66caca6f
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-03-14 10:16:12 +0530

    Created Hero Section and made some changes in notfound.js file

--- Task 1 Portfolio/myportfolio/app/not-found.js
+++ Task 1 Portfolio/myportfolio/app/not-found.js
@@ -9,8 +9,6 @@ const Error404 = () => {
 		<div class="col-sm-10 col-sm-offset-1  text-center">
 		<div class="four_zero_four_bg">
 			<h1 class="text-center font-Rosemary max-md:text-lg text-black">404 Page not found!</h1>
-		
-		
 		</div>
 		
 		<div class="contant_box_404 font-Rosemary text-black">
