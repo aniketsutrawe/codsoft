732692a66f4326a1e5ea91f98a6873771a9a204a
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-03-15 13:48:28 +0530

    added footer in website

---
+++ Task 1 Portfolio/myportfolio/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="p-12 flex justify-between font-logo text-xl">
+        <span>Aniket Sutrawe</span>
+        <p className=" text-primary-400 ">All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
